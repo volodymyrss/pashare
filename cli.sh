c6d19ab2cd59cc1215b19b3d474d123b3a7431f6
Volodymyr Savchenko <contact@volodymyrsavchenko.com>
2021-10-01 12:42:47 +0200

    change default port

--- cli.sh
+++ cli.sh
@@ -2,7 +2,7 @@
 case "$1" in
   start)
     $0 stop 
-    pactl load-module module-simple-protocol-tcp rate=44100 format=s16le channels=2 source=alsa_output.pci-0000_00_1f.3.analog-stereo.monitor record=true port=8000
+    pactl load-module module-simple-protocol-tcp rate=44100 format=s16le channels=2 source=alsa_output.pci-0000_00_1f.3.analog-stereo.monitor record=true port=8021
     ;;
   stop)
     pactl unload-module `pactl list | grep tcp -B1 | grep M | sed 's/[^0-9]//g'`
